dbbb16a532e292ef2d76874025d6f092199a0f82
Smeza314 <sebastianmeza314@gmail.com>
2021-03-17 10:13:04 -0700

    code

--- Develop/app.js
+++ Develop/app.js
@@ -9,13 +9,149 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { resolve } = require("path");
 
-const employees = [new Intern('John Doe', 1 , 'johndoe@gmail.com', 'USC'),
-new Intern('Jane Doe', 2, 'janedoe@gmail.com', 'UCLA')]
+let team = []
 
-fs.writeFile(outputPath, render(employees), err => {
-  if (err) { console.log(err) }
-})
+const engineer = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is your id?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your github?'
+    },
+  ])
+    .then(engineer => {
+      team.push(new Engineer(engineer.name, engineer.id, engineer.email, engineer.github))
+      role()
+    })
+    .catch(err => console.log(err))
+
+}
+
+const intern = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is your id?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?'
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'what school did you go to?'
+    }
+  ])
+    .then(intern => {
+      team.push(new Intern(intern.name, intern.id, intern.email, intern.school))
+      role()
+    })
+    .catch(err => console.log(err))
+}
+
+const manager = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is your id?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?'
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'What is your officeNumber?'
+    },
+  ])
+    .then(manager => {
+      team.push(new Manager(manager.name, manager.id, manager.email, manager.officeNumber))
+      role()
+    })
+    .catch(err => console.log(err))
+}
+
+const role = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'teamList',
+      choices: ["Engineer", "Intern", "Manager", "Finished"],
+      message: 'Pick which role you want to add to the list'
+    }
+  ])
+    .then(({ teamList }) => {
+      switch (teamList) {
+        case "Engineer":
+          engineer()
+          break;
+        case "Intern":
+          intern()
+          break;
+        case "Manager":
+          manager()
+          break;
+        case "Finished":
+          createHTML()
+          break;
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+const createHTML = () => {
+  console.log('creating html')
+  console.log(render(team))
+  render(team)
+  fs.writeFile(outputPath, render(team), err => {
+    if (err) { console.log(err) }
+  })
+}
+
+role()
+
+
+
+
+// const employees = [new Intern('John Doe', 1, 'johndoe@gmail.com', 'USC'),
+// new Intern('Jane Doe', 2, 'janedoe@gmail.com', 'UCLA')]
+
+// fs.writeFile(outputPath, render(employees), err => {
+//   if (err) { console.log(err) }
+// })
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Team-Profile-Generator
+
+This app will allow you to make a team by adding members you wish to a team roster. To use this app all you have to do is locate "app.js" and open the terminal and run the command "node app.js" it will then prompt you with some questions and once you complete it, you will get a team html file in the output folder which will display your team.
